31b8cedab455b6572b7c0a64e04466acd072b04c
aicxc <15999966670@163.com>
2019-07-30 11:05:03 +0800

    2019年7月30日11:04:58

---
+++ abstract-factory/src/main/java/com/aicxc/abstractfactory/IPhone.java
@@ -0,0 +1,8 @@
+package com.aicxc.abstractfactory;
+
+/**
+ * @author Hyy
+ * @version 2019/07/30 9:56
+ */
+public interface IPhone {
+}
---
+++ abstract-factory/src/main/java/com/aicxc/abstractfactory/XiaoMiRouter.java
@@ -0,0 +1,8 @@
+package com.aicxc.abstractfactory;
+
+/**
+ * @author Hyy
+ * @version 2019/07/30 10:24
+ */
+public class XiaoMiRouter {
+}
